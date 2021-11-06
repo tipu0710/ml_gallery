bb3e26b5898958579ada648899535f7e0f24cca3
Tipu Sultan <tsultan0710@gmail.com>
2021-11-06 11:31:44 +0600

    fix: clean up the project and added  const value
    1. added constans value
    2. added added responsive widget

---
+++ lib/utils/constants.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const baseUrl = "https://picsum.photos";
+const scaffoldColor = Color(0xFFfffef4);
+const appBarColor = Color(0xFFfbf9ef);
+const kPrimaryColor = Color(0xFF555539);
+
+const MaterialColor kPrimarySwatch = MaterialColor(
+  0xFF555539,
+  <int, Color>{
+    50: kPrimaryColor,
+    100: kPrimaryColor,
+    200: kPrimaryColor,
+    300: kPrimaryColor,
+    400: kPrimaryColor,
+    500: kPrimaryColor,
+    600: kPrimaryColor,
+    700: kPrimaryColor,
+    800: kPrimaryColor,
+    900: kPrimaryColor,
+  },
+);
